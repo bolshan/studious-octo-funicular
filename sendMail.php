bd98e82c151393d2bca4f9c6549557a28ced88a6
bolshan <132398739+bolshan@users.noreply.github.com>
2023-05-07 22:34:05 +0300

    Update sendMail.php

--- sendMail.php
+++ sendMail.php
@@ -2,10 +2,10 @@
 
 //------------Note You must use paid cpanel hosting ---------------
 
-$domainName = "website.com"; // Enter Domain Name Here
-$webmail ="info@website.com"; // Enter Webmail Email
-$webmailPass = "123456";  //Enter Webmail Password
-$recieverMail ="test@gmail.com"; // Your reciever email address
+$domainName = "gmail.com"; // Enter Domain Name Here
+$webmail ="etinene.ortega@gmail.com"; // Enter Webmail Email
+$webmailPass = "vpactbwdkxvkykre";  //Enter Webmail Password
+$recieverMail ="xianqzhou.han@gmail.com"; // Your reciever email address
 
 
 //----------------------------------
